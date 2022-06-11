d44b192b41eaf52e3fb8b51b3c309ad5f6eedf92
Colin Blokkeel <cblokkeel@nextoo.fr>
2022-06-11 11:49:42 +0200

    use LocalDateTime instead of String for publicationDate

--- technical-test-api/src/main/java/technical/test/api/entity/Book.java
+++ technical-test-api/src/main/java/technical/test/api/entity/Book.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -13,7 +14,7 @@ public class Book {
     @Id
     private UUID id;
     private String title;
-    private String publicationDate;
+    private LocalDateTime publicationDate;
     private Author author;
 
 }
