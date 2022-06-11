51efac4b9011eb7c043f96e27c84732c9acefbd2
Colin Blokkeel <cblokkeel@nextoo.fr>
2022-06-11 11:52:52 +0200

    add BookDTO

---
+++ technical-test-api/src/main/java/technical/test/api/dto/BookDTO.java
@@ -0,0 +1,30 @@
+package technical.test.api.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import technical.test.api.entity.Book;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+public class BookDTO {
+
+    private UUID id;
+    private String title;
+    private String authorName;
+    private LocalDateTime publicationDate;
+
+    public static BookDTO fromEntity(Book bookEntity) {
+        BookDTO bookDTO = new BookDTO();
+
+        bookDTO.setId(bookEntity.getId());
+        bookDTO.setTitle(bookEntity.getTitle());
+        bookDTO.setPublicationDate(bookEntity.getPublicationDate());
+        bookDTO.setAuthorName(bookEntity.getAuthor().getName());
+
+        return bookDTO;
+    }
+
+}
