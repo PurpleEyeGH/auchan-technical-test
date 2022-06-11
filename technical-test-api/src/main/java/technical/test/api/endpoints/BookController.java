d5d537972d05ab42724ac7bd4af27ce69e1c609b
Colin Blokkeel <cblokkeel@nextoo.fr>
2022-06-11 12:04:31 +0200

    add Book Controller

---
+++ technical-test-api/src/main/java/technical/test/api/endpoints/BookController.java
@@ -0,0 +1,28 @@
+package technical.test.api.endpoints;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import technical.test.api.dto.BookDTO;
+import technical.test.api.services.BookService;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Flux<BookDTO>> books() {
+        return ResponseEntity.ok(bookService.getBooks());
+    }
+
+}
