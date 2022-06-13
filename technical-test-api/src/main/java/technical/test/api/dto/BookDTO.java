9a615a9efcecd2deec0868a1697c5a4ae4a239c2
Colin Blokkeel <cblokkeel@nextoo.fr>
2022-06-13 16:38:20 +0200

    add to entity method and author gender

--- technical-test-api/src/main/java/technical/test/api/dto/BookDTO.java
+++ technical-test-api/src/main/java/technical/test/api/dto/BookDTO.java
@@ -2,9 +2,10 @@ package technical.test.api.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import technical.test.api.data.Gender;
 import technical.test.api.entity.Book;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -13,17 +14,30 @@ public class BookDTO {
     private String id;
     private String title;
     private String authorName;
-    private LocalDateTime publicationDate;
+    private String authorGender;
+    private String publicationDate;
 
     public static BookDTO fromEntity(Book bookEntity) {
         BookDTO bookDTO = new BookDTO();
 
         bookDTO.setId(bookEntity.getId());
         bookDTO.setTitle(bookEntity.getTitle());
-        bookDTO.setPublicationDate(bookEntity.getPublicationDate());
+        bookDTO.setPublicationDate(bookEntity.getPublicationDate().toString());
         bookDTO.setAuthorName(bookEntity.getAuthor().getName());
+        bookDTO.setAuthorGender(bookEntity.getAuthor().getGender().genderName);
 
         return bookDTO;
+    }
+
+    public static Book toEntity(BookDTO bookDTO) {
+        Book bookEntity = new Book();
+
+        bookEntity.setId(bookDTO.getId());
+        bookEntity.setTitle(bookDTO.getTitle());
+        bookEntity.setAuthor(new AuthorDTO(bookDTO.getAuthorName(), Gender.valueOf(bookDTO.getAuthorGender().trim().toUpperCase())));
+        bookEntity.setPublicationDate(LocalDate.parse(bookDTO.getPublicationDate()));
+
+        return bookEntity;
     }
 
 }
