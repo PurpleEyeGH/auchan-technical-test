1cad80703d6e107b90adcda528714673dd31eb09
Colin Blokkeel <cblokkeel@nextoo.fr>
2022-06-13 16:38:33 +0200

    add method to save a book in mongodb

--- technical-test-api/src/main/java/technical/test/api/service/BookService.java
+++ technical-test-api/src/main/java/technical/test/api/service/BookService.java
@@ -1,8 +1,9 @@
-package technical.test.api.services;
+package technical.test.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import technical.test.api.dto.BookDTO;
 import technical.test.api.repository.BookRepository;
 
@@ -18,6 +19,10 @@ public class BookService {
 
     public Flux<BookDTO> getBooks() {
         return bookRepository.findAll().map(BookDTO::fromEntity);
+    }
+
+    public Mono<BookDTO> saveBook(Mono<BookDTO> bookDTOMono) {
+        return bookDTOMono.map(BookDTO::toEntity).flatMap(bookRepository::insert).map(BookDTO::fromEntity);
     }
 
 }
